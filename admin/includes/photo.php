f06678bd4da6aae22347de9ca3a1621a45f5cffa
Maros Filip <maros.filip@gmail.com>
2016-03-04 09:41:35 +0100

    photo.php and photos.php edited

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -43,6 +43,11 @@ class Photo extends Db_object {
 		
 	} // End of set_file method
 
+	public function picture_path()
+	{
+		return $this->upload_directory.DS.$this->filename;
+	}
+
 	public function save() {
 		if ($this->photo_id) {
 			$this->update();
